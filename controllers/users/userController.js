de35e450a7d8f378d7d8da47fef4aa4f063834b8
dhruv patel <d7511162@gmail.com>
2024-12-08 22:13:57 +0530

    google bug fixed

--- controllers/users/userController.js
+++ controllers/users/userController.js
@@ -86,8 +86,8 @@ const userController = {
         //set the token into the cooke
         res.cookie("token", token, {
           httpOnly: true,
-          secure: false,
-          sameSite: "strict",
+          secure: true,
+          sameSite: "None",
           maxAge: 24 * 60 * 60 * 1000, //1 day:
         });
         //redirect the user dashboard
